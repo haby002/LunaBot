4e2d8729dae439b58a03384797599269ac26d644
Javier E. De la Garza Sanchez <javer.delagarza@gmail.com>
2017-11-19 16:05:13 -0800

    updated mysql query

--- LunaBot/Database/DiscordContext.cs
+++ LunaBot/Database/DiscordContext.cs
@@ -13,6 +13,6 @@ namespace LunaBot.Database
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder
-                .UseMySql(string.Format(@"Server=localhost;database={0};uid={1};pwd={2};", SecretStrings.database, SecretStrings.user, SecretStrings.password));
+                .UseMySql(string.Format(@"Server=localhost;database={0};uid={1};", SecretStrings.database, SecretStrings.user));
     }
 }
