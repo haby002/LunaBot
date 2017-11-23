5e3ae779646fb3ef2112ba75ede4085f18f613e5
Javier E. De la Garza Sanchez <javer.delagarza@gmail.com>
2017-11-22 21:22:20 -0800

    added timeout, made level up harder to get, added mute flavor text.

---
+++ LunaBot/Commands/Moderator/TimoutCommand.cs
@@ -0,0 +1,57 @@
+﻿using Discord.WebSocket;
+using LunaBot.Database;
+using LunaBot.ServerUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LunaBot.Commands
+{
+    [LunaBotCommand("Timeout")]
+    class TimeoutAllCommand : BaseCommand
+    {
+        public async override void Process(SocketMessage message, string[] parameters)
+        {
+            using (DiscordContext db = new DiscordContext())
+            {
+                long userId = Convert.ToInt64(message.Author.Id);
+                if (db.Users.Where(x => x.DiscordId == userId).FirstOrDefault().Privilege == 0)
+                {
+                    Logger.Warning(message.Author.Username, "Failed timout command. Not enough privileges.");
+                    await message.Channel.SendMessageAsync("You're not a moderator, go away.");
+
+                    return;
+                }
+
+                // Sanity check
+                if(message.MentionedUsers.Count == 0)
+                {
+                    Logger.Warning(message.Author.Username, "Failed timout command. No mentioned user.");
+                    await message.Channel.SendMessageAsync("No mentioned user. !timeout <user> <time>");
+
+                    return;
+                }
+
+                if(parameters.Length < 2)
+                {
+                    Logger.Warning(message.Author.Username, "Failed timout command. Time given.");
+                    await message.Channel.SendMessageAsync("Please specify an amount of time. !timeout <user> <time>");
+
+                    return;
+                }
+
+                if (!int.TryParse(parameters[1], out int seconds))
+                {
+                    Logger.Warning(message.Author.Username, "Failed timout command. Time for timout failed.");
+                    await message.Channel.SendMessageAsync("Time requested not a number. !timeout <user> <time>");
+
+                    return;
+                }
+
+                MuteUserHelper.mute(message.Channel as SocketTextChannel, message.MentionedUsers.FirstOrDefault() as SocketGuildUser, seconds);
+
+            }
+        }
+
+    }
+}
---
+++ LunaBot/ServerUtilities/MuteUserHelper.cs
@@ -0,0 +1,54 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LunaBot.ServerUtilities
+{
+    class MuteUserHelper
+    {
+        private static IList<string> kickFlavorText = new List<string>()
+        {
+            "You've said enough <@{0}>",
+            "Your colloquialism isn't welcomed for now <@{0}>",
+            "BAM! <@{0}> has been muted!",
+            "Don't want to lose that mouth, do you <@{0}>?",
+            "Chillax my dude. <@{0}> muted.",
+            "A wise man once said... hush <@{0}>"
+        };
+
+
+        public static async void mute(SocketTextChannel channel, SocketGuildUser user, int seconds)
+        {
+            if (user.Id == 333285108402487297)
+                return;
+
+            // Logging, telling the user, and announcing in server.
+            Logger.Info("System", $"Muting {user.Username}");
+            await user.SendMessageAsync($"You have been muted for {seconds} seconds");
+            Random r = new Random();
+            await channel.SendMessageAsync(String.Format(kickFlavorText[r.Next(kickFlavorText.Count)], user.Id));
+
+            Predicate<SocketRole> muteFinder;
+            SocketRole mute;
+            List<SocketRole> roles = channel.Guild.Roles.ToList();
+
+            // Set mute role
+            muteFinder = (SocketRole sr) => { return sr.Name == "Mute"; };
+            mute = roles.Find(muteFinder);
+            await user.AddRoleAsync(mute);
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(async () =>
+            {
+                Thread.Sleep(seconds * 1000);
+                await user.RemoveRoleAsync(mute);
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+    }
+}
