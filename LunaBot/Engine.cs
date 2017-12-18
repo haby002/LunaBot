a676026ea28350ef824cb788b2c60d7df8b978a4
Javier E. De la Garza S <jadelaga@microsoft.com>
2017-12-18 10:04:09 -0800

    removed emoji and left flip

--- LunaBot/Engine.cs
+++ LunaBot/Engine.cs
@@ -227,7 +227,7 @@ namespace LunaBot
                     {
                         ProcessXpAsync(message).ConfigureAwait(false);
                     }
-                    if (message.Content.Contains("(╯°□°）╯︵ ┻━┻)"))
+                    if (message.Content.Contains("︵ ┻━┻"))
                         message.Channel.SendMessageAsync($"┬─┬﻿ ノ( ゜-゜ノ) <@{message.Author.Id}>, please respect the furniture.").ConfigureAwait(false);
                 }).ConfigureAwait(false);
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
