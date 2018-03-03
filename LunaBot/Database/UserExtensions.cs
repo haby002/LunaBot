5c0a5483cf9829b89a558f2525975effafaaa942
Javier E. De la Garza Sanchez <javer.delagarza@gmail.com>
2018-03-03 18:21:58 -0500

    nickname reset fixed. Closed #27

--- LunaBot/Database/UserExtensions.cs
+++ LunaBot/Database/UserExtensions.cs
@@ -33,6 +33,7 @@ namespace LunaBot.Database
         {
             try
             {
+                user.Nickname = "";
                 user.Age = 0;
                 user.Description = null;
                 user.Fur = null;
