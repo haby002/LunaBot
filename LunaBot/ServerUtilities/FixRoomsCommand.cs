dc5b4f1dca8fac8f31c493f638ed6a16083f5705
Javier Enrique De la Garza Sanchez <javier.delagarza@gmail.com>
2018-04-04 11:00:01 -0700

    removed old commands

--- LunaBot/ServerUtilities/FixRoomsCommand.cs
+++ LunaBot/ServerUtilities/FixRoomsCommand.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Linq;
 using Discord.WebSocket;
 using LunaBot.Database;
@@ -25,7 +25,7 @@ namespace LunaBot.Commands
                 var guildChannels = channel.Guild.TextChannels;
                 foreach(SocketTextChannel ch in guildChannels)
                 {
-                    if (/*ch.Name.Contains("room-") || */ch.Name.Contains("void-") || ch.Name.Contains("intro-"))
+                    if (/*ch.Name.Contains("room-") || *//*ch.Name.Contains("void-") || ch.Name.Contains("intro-"))
                     {
                         Logger.Verbose("system", $"Found: {ch.Name}");
                         await ch.DeleteAsync();
@@ -87,3 +87,4 @@ namespace LunaBot.Commands
         }
     }
 }
+*/
---
+++ LunaBot/ServerUtilities/UserUtilities.cs
@@ -0,0 +1,44 @@
+﻿using Discord.WebSocket;
+using LunaBot.Database;
+using System;
+using System.Linq;
+
+namespace LunaBot.ServerUtilities
+{
+    internal static class UserUtilities
+    {
+        public static bool manualRegister(SocketGuildUser user)
+        {
+            using (DiscordContext db = new DiscordContext())
+            {
+                ulong userId = user.Id;
+                if (db.Users.Where(x => x.DiscordId == userId).Count() != 0)
+                {
+                    Logger.Verbose("System", $"User {user.Username} already registered");
+
+                    return false;
+                }
+
+                Logger.Verbose("System", $"Creating User {user.Username} Data");
+
+                User newUser = new User();
+                newUser.Nickname = "";
+                newUser.DiscordId = userId;
+                newUser.Level = 1;
+                newUser.LastMessage = DateTime.UtcNow;
+                newUser.Privilege = 0;
+                newUser.TutorialFinished = false;
+                newUser.Gender = User.Genders.None;
+                newUser.SnugG = 0;
+                newUser.SnugR = 0;
+                db.Users.Add(newUser);
+
+                db.SaveChanges();
+
+                Logger.Verbose("System", $"Created user {user.Username} successfully");
+
+                return true;
+            }
+        }
+    }
+}
