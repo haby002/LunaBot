4ec1b7b93db06682cf72099d8a703b87837404c0
Javier <javier.delagarza@gmail.com>
2018-08-07 13:43:36 -0700

    sorry moff

--- LunaBot/Engine.cs
+++ LunaBot/Engine.cs
@@ -456,7 +456,7 @@ namespace LunaBot
                 await message.Channel.SendMessageAsync("No awoo'ing allowed! You have been fined $" + match.Length * 12.50 + ".\n" +
                     "*We also accept dog biscuits.*");
             }
-            else if(Regex.IsMatch(message.Content, @"\bbuldge\b"))
+            else if(Regex.IsMatch(message.Content.ToLower(), @"\bbulge\b"))
             {
                 await BotReporting.ReportAsync(ReportColors.modCommand, message.Channel as SocketTextChannel, "Secret activated!", $"Activating message: {message.Content}", luna);
 
