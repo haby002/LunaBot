6e7fcf26966d27ea3573d2812d3ea7580d1db286
haby002 <javier_tennis1@hotmail.com>
2019-03-04 10:15:15 -0800

    Added personal room id saving to database

--- LunaBot/ServerUtilities/RoomUtilities.cs
+++ LunaBot/ServerUtilities/RoomUtilities.cs
@@ -7,7 +7,7 @@ namespace LunaBot.ServerUtilities
 {
     class RoomUtilities
     {
-        public static async Task CreatePersonalRoomAsync(SocketGuild guild, SocketGuildUser user)
+        public static async Task<RestTextChannel> CreatePersonalRoomAsync(SocketGuild guild, SocketGuildUser user)
         {
             SocketRole everyone = guild.Roles.Where(x => x.IsEveryone).First();
 
@@ -23,6 +23,8 @@ namespace LunaBot.ServerUtilities
                 $"The server might be public but this is your own private sliver of the server.\n" +
                 $"You can run commands, save images, post stuff, etc.\n" +
                 $"type `!help` for a list of the commands!");
+
+            return personalRoom;
         }
     }
 }
